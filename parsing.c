ddf4783f9432acf0f59835254d0cf56c60d5e6e9
BJ Neilsen <bj.neilsen@gmail.com>
2023-02-18 09:55:21 -0500

    ch7 : recursive eval

--- parsing.c
+++ parsing.c
@@ -22,10 +22,38 @@ void add_history(char* unused) {}
 #include <editline/readline.h>
 #endif
 
+long eval_op(long x, char* op, long y) {
+  if (strcmp(op, "+") == 0 || strcmp(op, "add") == 0) { return x + y; }
+  if (strcmp(op, "-") == 0 || strcmp(op, "sub") == 0) { return x - y; }
+  if (strcmp(op, "*") == 0 || strcmp(op, "mul") == 0) { return x * y; }
+  if (strcmp(op, "/") == 0 || strcmp(op, "div") == 0) { return x / y; }
+  return 0;
+}
 
 /*
- * lithp Grammar Definition
- */
+ * typedef struct mpc_ast_t {
+ *   char* tag;
+ *   char* contents;
+ *   mpc_state_t state;
+ *   int children_num;
+ *   struct mpc_ast_t** children;
+ * } mpc_ast_t;
+*/
+long eval(mpc_ast_t* t) {
+ if (strstr(t->tag, "number")) {
+   return atoi(t->contents);
+ }
+ char* op = t->children[1]->contents;
+ long x = eval(t->children[2]);
+
+ int i = 3;
+ while (strstr(t->children[i]->tag, "expr")) {
+   x = eval_op(x, op, eval(t->children[i]));
+   i++;
+ }
+
+ return x;
+}
 
 int main(int argc, char** argv) {
   mpc_parser_t* Number = mpc_new("number");
@@ -50,6 +78,8 @@ int main(int argc, char** argv) {
     mpc_result_t r;
     if (mpc_parse("<stdin>", input, Lithp, &r)) {
       mpc_ast_print(r.output);
+      long result = eval(r.output);
+      printf("%li\n", result);
       mpc_ast_delete(r.output);
     } else {
       mpc_err_print(r.error);
