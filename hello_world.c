529dd05a80e91eb145364d54fb4bd912504f811f
BJ Neilsen <bj.neilsen@gmail.com>
2024-03-19 05:46:42 -0400

    code formatting

--- hello_world.c
+++ hello_world.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-#define string char*
+#define string char *
 
-void loop(int n, void (*fn)()) {
+void times(int n, void (*fn)(int, int)) {
   for (int i = 0; i < n; i++) {
     fn(i, n);
   }
@@ -12,17 +12,17 @@ void say_hi(int index, int total) {
   printf("[%2d of %d] : Hello World\n", index, total);
 }
 
-void print_args(int argc, char** argv) {
+void print_args(int argc, char **argv) {
   for (int i = 0; i < argc; i++) {
     printf("arg %d: %s\n", i, argv[i]);
   }
 }
 
-int main(int argc, string* argv) {
+int main(int argc, string *argv) {
   print_args(argc, argv);
   printf("Hello,");
   string a = "world";
   printf(" %s!\n", a);
-  loop(25, say_hi);
+  times(25, say_hi);
   return 0;
 }
--- parsing.c
+++ parsing.c
@@ -8,25 +8,33 @@
 
 static char buffer[2048];
 
-char* readline(char* prompt) {
+char *readline(char *prompt) {
   fputs(prompt, stdout);
   fgets(buffer, 2048, stdin);
-  char* cpy = malloc(strlen(buffer)+1);
+  char *cpy = malloc(strlen(buffer) + 1);
   strcpy(cpy, buffer);
-  cpy[strlen(cpy)-1] = '\0';
+  cpy[strlen(cpy) - 1] = '\0';
 }
 
-void add_history(char* unused) {}
+void add_history(char *unused) {}
 
 #else
 #include <editline/readline.h>
 #endif
 
-long eval_op(long x, char* op, long y) {
-  if (strcmp(op, "+") == 0 || strcmp(op, "add") == 0) { return x + y; }
-  if (strcmp(op, "-") == 0 || strcmp(op, "sub") == 0) { return x - y; }
-  if (strcmp(op, "*") == 0 || strcmp(op, "mul") == 0) { return x * y; }
-  if (strcmp(op, "/") == 0 || strcmp(op, "div") == 0) { return x / y; }
+long eval_op(long x, char *op, long y) {
+  if (strcmp(op, "+") == 0 || strcmp(op, "add") == 0) {
+    return x + y;
+  }
+  if (strcmp(op, "-") == 0 || strcmp(op, "sub") == 0) {
+    return x - y;
+  }
+  if (strcmp(op, "*") == 0 || strcmp(op, "mul") == 0) {
+    return x * y;
+  }
+  if (strcmp(op, "/") == 0 || strcmp(op, "div") == 0) {
+    return x / y;
+  }
   return 0;
 }
 
@@ -38,41 +46,42 @@ long eval_op(long x, char* op, long y) {
  *   int children_num;
  *   struct mpc_ast_t** children;
  * } mpc_ast_t;
-*/
-long eval(mpc_ast_t* t) {
- if (strstr(t->tag, "number")) {
-   return atoi(t->contents);
- }
- char* op = t->children[1]->contents;
- long x = eval(t->children[2]);
+ */
+long eval(mpc_ast_t *t) {
+  if (strstr(t->tag, "number")) {
+    return atoi(t->contents);
+  }
+  char *op = t->children[1]->contents;
+  long x = eval(t->children[2]);
 
- int i = 3;
- while (strstr(t->children[i]->tag, "expr")) {
-   x = eval_op(x, op, eval(t->children[i]));
-   i++;
- }
+  int i = 3;
+  while (strstr(t->children[i]->tag, "expr")) {
+    x = eval_op(x, op, eval(t->children[i]));
+    i++;
+  }
 
- return x;
+  return x;
 }
 
-int main(int argc, char** argv) {
-  mpc_parser_t* Number = mpc_new("number");
-  mpc_parser_t* Operator = mpc_new("operator");
-  mpc_parser_t* Expr = mpc_new("expr");
-  mpc_parser_t* Lithp = mpc_new("lithp");
+int main(int _, char **_) {
+  mpc_parser_t *Number = mpc_new("number");
+  mpc_parser_t *Operator = mpc_new("operator");
+  mpc_parser_t *Expr = mpc_new("expr");
+  mpc_parser_t *Lithp = mpc_new("lithp");
 
   mpca_lang(MPCA_LANG_DEFAULT, "                        \
     number   : /-?[0-9]+(\\.[0-9]+)?/ ;                 \
     operator : /(-|+|*|\\/|%|add|sub|mul|div)/ ;        \
     expr     : <number> | '(' <operator> <expr>+ ')'  ; \
     lithp    : /^/ <operator> <expr>+ /$/ ;             \
-", Number, Operator, Expr, Lithp);
+",
+            Number, Operator, Expr, Lithp);
 
   puts("lithp version 0.0.0.0.1");
   puts("Press Ctrl+C to exit\n");
 
   while (1) {
-    char* input = readline("lithp> ");
+    char *input = readline("lithp> ");
     add_history(input);
 
     mpc_result_t r;
